1a16027a57f09388cd16fe87f4390fc318baed21
Witt Sparks <wittsparks@yahoo.com>
2016-01-29 13:44:17 -0500

    Fix the hover problem on touch devices

--- form.js
+++ form.js
@@ -1,4 +1,13 @@
 $(document).ready( function() {
+   function fix()
+   {
+       var el = this;
+       var par = el.parentNode;
+       var next = el.nextSibling;
+       par.removeChild(el);
+       setTimeout(function() {par.insertBefore(el, next);}, 0)
+   }
+
    /* Handle the menu button on mobile */
    $(".stack-menu-button").click( function() {
       if ( $(".stack-menu").css("display") == "none" ) 
