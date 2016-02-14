a177019756bf26eff48e19dc656bfaed2b53d882
Witt Sparks <wittsparks@yahoo.com>
2016-02-14 07:44:39 -0700

    Remove the old call to mail()

--- contact.php
+++ contact.php
@@ -13,10 +13,6 @@ $email
 ;
 
 $sendgrid->send($email);
-/*
-$result = mail("wittsparks@yahoo.com", "Email from wittsparks.com",
-   $_POST['name']." at ".$_POST['email']." says:<br/>".$_POST['message']);
-*/
 
 error_log("A contact form was submitted to wittsparks.com:");
 error_log(var_export($_POST, true));
