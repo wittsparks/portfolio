37ddb7e4bb0db3871c4329ece174c45900a3ade8
Witt Sparks <witt@izoox.com>
2020-06-28 09:03:23 -0600

    queen of calm

---
+++ queen/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<style>
+   body {
+      width: 100%;
+      font-family: arial, sans-serif;
+      text-align: center;
+      background-image: url('background.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      color:white;
+   }
+
+   h1 {
+      margin-top: 100px;
+   }
+</style>
+<script>
+</script>
+</head>
+<body>
+   <h1>Queen of Calm</h1>
+
+    <audio controls>
+     <source src="Queenofcalm.mp3" type="audio/mpeg">
+     Your browser does not support the audio element.
+    </audio>
+</body>
+</html>
