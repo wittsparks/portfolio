0d482bff039be29c42bdd77d13bed422b805fc84
Witt Sparks <witt@izoox.com>
2020-06-29 07:31:03 -0600

    add buttons

--- queen/index.php
+++ queen/index.php
@@ -16,20 +16,41 @@
       font-size: 300%;
    }
 
+   h2 {
+      display:inline-block;
+      width: 30%;
+      font-size: 48px;
+      height: 80px;
+   }
+
+   .buttons {
+      height:50px;
+      margin-bottom:100px;
+   }
+
    audio {
       margin:0 auto;
       width: 90%;
    }
+
+
 </style>
 <script>
 
    var play_audio = function() {
       document.getElementById('player').play();
    }
+   var rewind_audio = function() {
+      document.getElementById('player').currentTime=0;
+   }
 </script>
 </head>
 <body>
    <h1>Queen of Calm</h1>
+   <div class="buttons">
+      <h2 onclick="play_audio()">Play</h2>
+      <h2 onclick="rewind_audio()">Restart</h2>
+    </div> 
     <audio id="player" controls>
      <source src="Queenofcalm.m4a" type="audio/mp4">
      Your browser does not support the audio element.
